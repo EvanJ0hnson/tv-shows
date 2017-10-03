9c71cbdfb1239d95a20ed07ca8e8a5ed68ddfe53
EvanJ0hnson <ivangerasimow@gmail.com>
2017-10-03 04:31:23 +0300

    feat(AddShow) Add "prop-types"

--- src/AddShow/AddShow.js
+++ src/AddShow/AddShow.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import SearchableList from '../SearchableList/SearchableList';
 
 const title = 'Add Show';
@@ -10,5 +11,9 @@ const AddShow = ({ items }) => (
     <SearchableList items={items} />
   </div>
 );
+
+AddShow.propTypes = {
+  items: PropTypes.array.isRequired,
+};
 
 export default AddShow;
