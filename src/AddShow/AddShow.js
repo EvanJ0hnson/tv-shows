cc594d70f13a7bd011714deb5d41cabf4fda9cf5
EvanJ0hnson <ivangerasimow@gmail.com>
2017-10-03 04:31:05 +0300

    feat(AddShow) Use props to show data

--- src/AddShow/AddShow.js
+++ src/AddShow/AddShow.js
@@ -1,31 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SearchableList from '../SearchableList/SearchableList';
 
 const title = 'Add Show';
-const items = [{
-  title: 'Item 1',
-  plot: 'plot…',
-  prev: 'prev',
-  next: 'next',
-}, {
-  title: 'Item 2',
-  plot: 'plot…',
-  prev: 'prev',
-  next: 'next',
-}];
 
-class AddShow extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <h2>{title}</h2>
-        </header>
+const AddShow = ({ items }) => (
+  <div>
+    <h2>{title}</h2>
 
-        <SearchableList items={items} />
-      </div>
-    );
-  }
-}
+    <SearchableList items={items} />
+  </div>
+);
 
 export default AddShow;
